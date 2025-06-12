097716ef15f0c22e21a394608b8aa9f16ddf7fd6
hosa200 <hosam.m.yonis@gmail.com>
2025-06-12 00:23:25 +0000

    trigger an action

--- full-test.sh
+++ full-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script can be copied into your base directory for use with
-# automated testing using assignment-autotest.  It automates the
+# automated testing using assignment-autotest.  It automates the 
 # steps described in https://github.com/cu-ecen-5013/assignment-autotest/blob/master/README.md#running-tests
 set -e
 
